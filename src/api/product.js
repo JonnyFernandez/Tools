ea6ee45c463dbbae12ec9a7f346524ada68be2c1
JonnyFernandez <arcancode@gmail.com>
2024-12-25 14:46:21 -0300

    maqueta prod admin

--- src/api/product.js
+++ src/api/product.js
@@ -1,13 +1,13 @@
 import axios from './axios'
 
 const products = [
-    { id: '001', name: 'Producto 1', stock: 100, description: 'Descripción del producto 1', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 10 },
-    { id: '002', name: 'Producto 2', stock: 50, description: 'Descripción del producto 2', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_878129-MLA53164699794_012023-F.webp', price: 20 },
-    { id: '003', name: 'Producto 3', stock: 200, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_781792-MLA47133276673_082021-O.webp', price: 15 },
-    { id: '004', name: 'cloro liquido', stock: 200, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 760 },
-    { id: '005', name: 'cloro DL', stock: 200, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 7500 },
-    { id: '006', name: 'cloro DR', stock: 200, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 7500 },
-    { id: '007', name: 'cloro MF', stock: 200, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 7900 },
+    { id: '001', name: 'Producto 1', stock: 20, minStock: 20, description: 'Descripción del producto 1', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 10 },
+    { id: '002', name: 'Producto 2', stock: 0, minStock: 20, description: 'Descripción del producto 2', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_878129-MLA53164699794_012023-F.webp', price: 20 },
+    { id: '003', name: 'Producto 3', stock: 200, minStock: 20, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_781792-MLA47133276673_082021-O.webp', price: 15 },
+    { id: '004', name: 'cloro liquido', stock: 200, minStock: 20, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 760 },
+    { id: '005', name: 'cloro DL', stock: 200, minStock: 20, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 7500 },
+    { id: '006', name: 'cloro DR', stock: 200, minStock: 20, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 7500 },
+    { id: '007', name: 'cloro MF', stock: 200, minStock: 20, description: 'Descripción del producto 3', image: 'https://http2.mlstatic.com/D_NQ_NP_2X_895197-MLA81353946081_122024-F.webp', price: 7900 },
 ];
 
 // -------------------------Fake end point---------------------------------------------------
--- src/views/prodManagement/ProdManagement.jsx
+++ src/views/prodManagement/ProdManagement.jsx
@@ -6,25 +6,54 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const ProdManagement = () => {
     const dispatch = useDispatch();
-    const info = apiProd();
 
     useEffect(() => {
-        dispatch(getProd(info));
+        dispatch(getProd(apiProd()));
     }, [dispatch]);
 
     const { products } = useSelector(state => state.prod);
 
+    const getStockClass = (stock, minStock) => {
+        if (stock === 0) return p.notStock;
+        if (stock <= minStock) return p.lowStock;
+        return p.cardStock;
+    };
+
     return (
         <div className={p.prodManagement}>
             <h1 className={p.title}>Product Management</h1>
+            <div className={p.filterContainer}>
+                <h5 className={p.filterTitle}>Filter by:</h5>
+                <select className={p.filterSelect}>
+                    <option value="">Category</option>
+                </select>
+                <select className={p.filterSelect}>
+                    <option value="">Price</option>
+                </select>
+                <select className={p.filterSelect}>
+                    <option value="">Supplier</option>
+                </select>
+                <select className={p.filterSelect}>
+                    <option value="">Stock</option>
+                </select>
+                <select className={p.filterSelect}>
+                    <option value="">Price Management</option>
+                    <option value="">Categoría</option>
+                    <option value="">Distribuidor</option>
+                    <option value="">Globales </option>
+                    <option value="">Promociones  </option>
+                </select>
+                <button className={p.addProd}>Agregar Producto</button>
+            </div>
             <div className={p.cardContainer}>
-                {products?.map(product => (
-                    <div key={product.id} className={p.card}>
-                        <img src={product.image} alt={product.name} className={p.cardImage} />
+                {products?.map(({ id, image, name, description, stock, minStock, price }) => (
+                    <div key={id} className={p.card}>
+                        <img src={image} alt={name} className={p.cardImage} />
                         <div className={p.cardContent}>
-                            <h2 className={p.cardTitle}>{product.name}</h2>
-                            <p className={p.cardDescription}>{product.description}</p>
-                            <p className={p.cardPrice}>${product.price.toFixed(2)}</p>
+                            <h2 className={p.cardTitle}>{name}</h2>
+                            <p className={p.cardDescription}>{description}</p>
+                            <p className={getStockClass(stock, minStock)}>stock: {stock}</p>
+                            <p className={p.cardPrice}>${price.toFixed(2)}</p>
                         </div>
                     </div>
                 ))}
